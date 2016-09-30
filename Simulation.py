cbafb4d74485c504f6e6080ec846c8fde9c8d516
CHEMTRAILCHAMELION <user@Users-MacBook-Pro.local>
2016-09-30 13:59:37 +0100

    added to simulation

--- Simulation.py
+++ Simulation.py
@@ -3,21 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # def main():
-d = 2  # distance between song meters (m)
+d = 1  # distance between song meters (m)
 c = 343.2  # speed of sound (m/s)
 a = d/c  # time taken between posts
 
-degs = np.linspace(0, 2*np.pi, 181)  # create x axis array of degrees
+degs = np.linspace(0, np.pi, 91)  # create x axis array of degrees
 
-DegToRad = np.pi/180
-deg = 45*DegToRad
+DegToRad = np.pi/180 # convert degrees to radians
+deg = 45*DegToRad # find our angle in radians
 
-TOAdiff = np.cos(degs)
+TOAdiff = np.cos(degs)*d # get our TOA difference from the
+dyTOA = 10*np.gradient(TOAdiff,degs)
 
 ang = np.arccos(1)
 
-print(degs)
-print(TOAdiff)
+print(TOAdiff[0:5])
+print(dyTOA[0:5])
 
 plt.plot(TOAdiff)
+plt.plot(dyTOA)
 plt.show()
