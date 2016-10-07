c46135a4c7441f7076b8670062e402541b52ad09
CHEMTRAILCHAMELION <shaboier@gmail.com>
2016-10-07 11:31:48 +0100

    cant remember what i did in this one

--- Simulation.py
+++ Simulation.py
@@ -9,19 +9,25 @@ tMax = d/c  # Max time taken between posts. Time equals distance divided by spee
 
 degs = np.linspace(0, np.pi, 181)  # create array of radians from 0 to pi(180 degrees), 181 points, 1 per degree
 
-DegToRad = np.pi/180  # create metric to convert degrees to radians
-
 
 def toadiff(x):
     return np.cos(x)  # calculate the time of arrival difference
 
 
 # Calculate Change in TOADiff over angle
-dyTOADiff = abs(derivative(toadiff, degs, dx=1e-10))
+def dytoadiff(toad):
+    return abs(derivative(toad, degs, dx=1e-10))
 
+
+def d2ytoadiff(dytoad):
+    return abs(derivative(dytoad, degs, dx=1e-10))
+
+dyTOADiff = dytoadiff(toadiff)
+d2yTOADiff = d2ytoadiff(dytoadiff(toadiff))
 
 print(toadiff(degs), " = TOADiff")
 print(dyTOADiff, " = dyTOADiff")
 plt.plot(abs(toadiff(degs)))
 plt.plot(dyTOADiff)
+plt.plot(d2yTOADiff)
 plt.show()
